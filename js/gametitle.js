4386d1121f7a10f7524218e17baf7321f3795366
metalx1000 <metal_bullets@filmsbykris.com>
2014-10-08 15:33:09 -0400

    Added Letter Tweening

--- js/gametitle.js
+++ js/gametitle.js
@@ -2,22 +2,18 @@ var gameTitle = function(game){}
 
 gameTitle.prototype = {
   	create: function(){
+            this.load_paper();
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            var gameTitle = this.game.add.sprite(this.game.world.width * 0.5,this.game.world.height * .1,"game_title");
+            gameTitle.anchor.setTo(0.5,0.5);
 
-		background  = this.game.add.sprite(this.game.world.width * 0.5,this.game.world.height * 0.5,"background");
-                background.scale.setTo(2,2);
-		background.anchor.setTo(0.5,0.5);
+            var playButton = this.game.add.button(this.game.world.width * 0.5,this.game.world.height * .5,"play_btn",this.playTheGame,this);
+            playButton.anchor.setTo(0.5,0.5);
 
-                this.game.physics.startSystem(Phaser.Physics.ARCADE);
-		var gameTitle = this.game.add.sprite(this.game.world.width * 0.5,this.game.world.height * .1,"game_title");
-		gameTitle.anchor.setTo(0.5,0.5);
+            this.main_title();
 
-		var playButton = this.game.add.button(this.game.world.width * 0.5,this.game.world.height * .5,"play_btn",this.playTheGame,this);
-		playButton.anchor.setTo(0.5,0.5);
-
-                this.main_title();
-
-                //go full screen on click
-                this.game.input.onDown.add(this.fullscreen, this);
+            //go full screen on click
+            this.game.input.onDown.add(this.fullscreen, this);
 	},
         update: function(){
             
@@ -41,5 +37,9 @@ gameTitle.prototype = {
             window.open("http://filmsbykris.com", "_blank");
         },
 
-
-}   
+        load_paper: function(){    
+            background  = this.game.add.sprite(this.game.world.width * 0.5,this.game.world.height * 0.5,"background");
+            background.scale.setTo(2,2);
+            background.anchor.setTo(0.5,0.5);
+        }
+}
--- js/thegame.js
+++ js/thegame.js
@@ -1,8 +1,11 @@
 var theGame = function(game){
+    words = ["CAT","DOG"];
 }
 
 theGame.prototype = {
   	create: function(){
+            this.load_paper();
+            this.load_word();
             var click_me = this.game.add.button(this.game.world.width * 0.5,this.game.world.height * 0.2,"click_me",this.level1,this);
 	    click_me.anchor.setTo(0.5,0.5);
 	    this.create_player("playerf");
@@ -35,5 +38,28 @@ theGame.prototype = {
             tween = this.game.add.tween(player).to({ x: this.game.world.width * 0.5 }, 1000);
             tween.start();
 
-	}
+	},
+        load_paper: function(){
+            background  = this.game.add.sprite(this.game.world.width * 0.5,this.game.world.height * 0.5,"background");
+            background.scale.setTo(2,2);
+            background.anchor.setTo(0.5,0.5);
+        },
+        load_word: function(){
+            for(var i=0;i<words.length;i++){
+                var letter = this.game.add.sprite(200*i+128,this.game.world.height * 0.5,"L_img");
+                letter.anchor.setTo(.5,.5);
+                letter.scale.setTo(4,4);
+                letter.inputEnabled = true;
+                letter.events.onInputOver.add(function(_this){
+                    var tween = _this.game.add.tween(_this.scale).to({ x: 5, y: 5 }, 200);
+                    tween.start();
+                });
+                letter.events.onInputOut.add(function(_this){
+                    var tween = _this.game.add.tween(_this.scale).to({ x: 4, y: 4 }, 200);
+                    tween.start();
+                });
+
+            }
+        }
+
 }
