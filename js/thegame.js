e13782b0bb8f16d4a34445a948a864ff0c9456f3
Kris Occhipinti <metal_bullets@filmsbykris.com>
2014-10-12 14:23:38 -0400

    Worked on letter touching

--- js/thegame.js
+++ js/thegame.js
@@ -34,11 +34,10 @@ theGame.prototype = {
             var word = words[Math.floor(Math.random()*words.length)];
             var l = word.split("");
             for(var i=0;i<l.length;i++){
-                var letter = this.game.add.sprite(200*i+128,this.game.world.height * 0.5,l[i] + "_img");
+                var letter = this.game.add.button(200*i+128,this.game.world.height * 0.5,l[i] + "_img",null,this);
                 letter.snd = this.game.add.audio(l[i] +"_snd");
                 letter.anchor.setTo(.5,.5);
                 letter.scale.setTo(4,4);
-                letter.inputEnabled = true;
                 letter.events.onInputOver.add(function(_this){
                     _this.snd.play('',0,1,false);
                     var tween = _this.game.add.tween(_this.scale)
@@ -46,6 +45,14 @@ theGame.prototype = {
                     .to({ x: 4, y: 4 }, 400)
                     .start();
                 });
+                letter.events.onInputDown.add(function(_this){
+                    _this.snd.play('',0,1,false);
+                    var tween = _this.game.add.tween(_this.scale)
+                    .to({ x: 5, y: 5 }, 200)
+                    .to({ x: 4, y: 4 }, 400)
+                    .start();
+                });
+
 
 
             }
